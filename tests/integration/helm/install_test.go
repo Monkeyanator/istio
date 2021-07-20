499749f51ec33412abdeb2fea42510e4f815a108
Sam Naser <samnaser@google.com>
2021-07-20 11:40:59 -0400

    Add Helm test for validation

--- tests/integration/helm/install_test.go
+++ tests/integration/helm/install_test.go
@@ -35,6 +35,7 @@ func TestDefaultInstall(t *testing.T) {
 global:
   hub: %s
   tag: %s
+defaultRevision: "default"
 `
 	framework.
 		NewTest(t).
@@ -51,6 +52,7 @@ global:
   hub: %s
   tag: %s
   jwtPolicy: first-party-jwt
+defaultRevision: "default"
 `
 
 	framework.
@@ -81,6 +83,7 @@ func setupInstallation(overrideValuesStr string) func(t framework.TestContext) {
 		InstallIstio(t, cs, h, "", overrideValuesFile, ManifestsChartPath, "", true)
 
 		VerifyInstallation(t, cs, true)
+		VerifyValidation(t)
 
 		sanitycheck.RunTrafficTest(t, t)
 		t.Cleanup(func() {
